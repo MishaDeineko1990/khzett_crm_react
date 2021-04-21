730373493818dbd34f3d963cb371fb9c55690f3e
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2021-04-21 10:34:48 +0300

    new_try

--- my-app/src/components/Contents/Site_body/Zvit/Zvit.jsx
+++ my-app/src/components/Contents/Site_body/Zvit/Zvit.jsx
@@ -10,115 +10,52 @@ import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import Wrapp_zvit from './Wrapp_zvit/Wrapp_zvit';
 import XLSX from 'xlsx';
+import XLS from 'xlsx';
 
 
 const Zvit = () =>{
 
 
 
-        
-
-
-    function debag_encode (data){
-
-        var result = []
-    
-        data.forEach(its => {
-            let el = its 
-
-            console.log(el)
-    
-            // let check = {
-            // é: ["é", "й"], ö: ["ö", "ц"], ó: ["ó", "у"], ê: ["ê", "к"], å: ["å", "е"], í: ["í", "н"], ã: ["ã", "г"], ø: ["ø", "ш"], ù: ["ù", "щ"], ç: ["ç", "з"], õ: ["õ", "х"], ô: ["ô", "ф"], â: ["â", "в"], à: ["à", "а"], ï: ["ï", "п"], ð: ["ð", "р"], î: ["î", "о"], ë: ["ë", "л"], ä: ["ä", "д"], æ: ["æ", "ж"], º: ["º", "є"], ÿ: ["ÿ", "я"], ñ: ["ñ", "с"], ì: ["ì", "м"], è: ["è", "и"], ò: ["ò", "т"], ü: ["ü", "ь"], á: ["á", "б"], þ: ["þ", "ю"], É: ["É", "Й"], Ö: ["Ö", "Ц"], Ó: ["Ó", "У"], Ê: ["Ê", "К"], Å: ["Å", "Е"], Í: ["Í", "Н"], Ã: ["Ã", "Г"], Ø: ["Ø", "Ш"], Ù: ["Ù", "Щ"], Ç: ["Ç", "З"], Õ: ["Õ", "Х"], Ô: ["Ô", "Ф"], Â: ["Â", "В"], À: ["À", "А"], Ï: ["Ï", "П"], Ð: ["Ð", "Р"], Î: ["Î", "О"], Ë: ["Ë", "Л"], Ä: ["Ä", "Д"], Æ: ["Æ", "Ж"], ª: ["ª", "Є"], ß: ["ß", "Я"], Ñ: ["Ñ", "С"], Ì: ["Ì", "М"], È: ["È", "И"], Ò: ["Ò", "Т"], Ü: ["Ü", "Ь"], Á: ["Á", "Б"], Þ: ["Þ", "Ю"]
-            // }
-    
-            // el = el.replace(/[éöóêåíãøùçõôâàïðîëäæÿñìèòüáþÉÖÓÊÅÍÃØÙÇÕÔÂÀÏÐÎËÄÆßÑÌÈÒÜÁÞ]/g, (a1) => {           
-                
-    
-            //     if (check[a1]){
-            //         return check[a1][1]
-            //     }
-                
-            // })
-    
-            // el = el.replace(/[³]/g, "і")
-            // el = el.replace(/[¿]/g, "ї")
-            // el = el.replace(/[º]/g, "є")
-            // el = el.replace(/[÷]/g, "ч")
-            // el = el.replace(/[¯]/g, "Ї")
-            // el = el.replace(/[²]/g, "І")
-            // el = el.replace(/[ª]/g, "Є")
-            // el = el.replace(/[×]/g, "Ч")
-    
-            // for(let i=0; i<10; i++){
-            //     el = el.replace(/  /g, " ")
-            // }
             
-            // el = el.replace(" Шнекова", "Шнекова")
-    
-            // result.push(el)
-    
-        })
-        
-    // console.log(result); 
-    }
-    
-    
-    
+
+    var oFileIn;
+
+    $(function() {
+        oFileIn = document.getElementById('my_file_input');
+        if(oFileIn.addEventListener) {
+            oFileIn.addEventListener('change', filePicked, false);
+        }
+    });
     
     
-    // let data = [
-    //     "    Øíåêîâà ñï³ðàëü 225*76*220  S-4 L-2,0ì ë³âà 01300",
-    //     "6666 ",
-    //     "   Øíåêîâà ñï³ðàëü 225*76*220  S-4 L-2,0ì ë³âà 01300 ",
-    //     "kjkk ",
-    //     " ",
-    //     " ",
-    //     " ",
-    //     " ",
-    //     " "
-    //     ]    
+    function filePicked(oEvent) {
+        // Get The File From The Input
+        var oFile = oEvent.target.files[0];
+        var sFilename = oFile.name;
+        // Create A File Reader HTML5
+        var reader = new FileReader();
     
+        // Ready The Event For When A File Gets Selected
+        reader.onload = function(e) {
+            var data = e.target.result;
+            var cfb = XLS.CFB.read(data, {type: 'binary'});
+            var wb = XLS.parse_xlscfb(cfb);
+            // Loop Over Each Sheet
+            wb.SheetNames.forEach(function(sheetName) {
+                // Obtain The Current Row As CSV
+                var sCSV = XLS.utils.make_csv(wb.Sheets[sheetName]);   
+                var oJS = XLS.utils.sheet_to_row_object_array(wb.Sheets[sheetName]);   
     
-    // debag_encode(data)
-    
-    
-    $(document).ready(function(){
-        $("#fileUploader").change(function(evt){
-            var selectedFile = evt.target.files[0];
-            var reader = new FileReader();
-            reader.onload = function(event) {
-            var data = event.target.result;
-            var workbook = XLSX.read(data,{
-                type: 'binary'
+                $("#my_file_output").html(sCSV);
+                console.log(oJS)
             });
-            workbook.SheetNames.forEach(function(sheetName){       
-                var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-                var json_object = JSON.stringify(XL_row_object);
-                
-                
-                // json_object
-                // json_object.forEach( its=> {
-                   let ii =  JSON.parse(json_object)
+        };
+    
+        // Tell JS To Start Reading The File.. You could delay this if desired
+        reader.readAsBinaryString(oFile);
+    }
 
-
-// *********************problemm for hash
-
-                   ii.forEach(its => {
-                    console.log(JSON.parse(its))
-                   })
-                   
-                // })               
-            })
-            };
-            reader.onerror = function(event) {
-            console.error("Файл не может быть прочитан. Код ошибки: " + event.target.error.code);
-            };
-            reader.readAsBinaryString(selectedFile);
-                });
-                });
-      
-   
 
 
 
@@ -130,17 +67,16 @@ const Zvit = () =>{
             <div>    
                 <NavLink to ="/Zvit/Wrapp_zvit">Shov zvit</NavLink>
                 <span></span>
-                <NavLink to ="/Zvit/Wrapp_zvit">   Shov zvit</NavLink>
-                <span></span>
-                <NavLink to ="/Zvit/Wrapp_zvit">    Shov zvit</NavLink>
-                <span></span>
-                <NavLink to ="/Zvit/Wrapp_zvit">    Shov zvit</NavLink>
-                <span></span>
-                <NavLink to ="/Zvit/Wrapp_zvit">    Shov zvit</NavLink>
-                <input type="file" id="fileUploader" name="fileUploader" accept=".xls, .xlsx"></input>
+
+            </div>                
+
+            <div>
+                <input type="file" id="my_file_input" />                
             </div>
 
-                <Wrapp_zvit/>
+            <Wrapp_zvit/>
+
+            <div id='my_file_output'></div> 
 
 
             
